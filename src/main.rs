44d7d4c64bfe61b75b962c1550cc745b465cced7
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-26 09:58:29 +0200

    classic hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> iced::Result {
+    iced::run("Chess against engine", App::update, App::view)
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+#[derive(Default)]
+struct App {}
+
+impl App {
+    fn update(&mut self, _message: Message) {}
+
+    fn view(&self) -> iced::Element<Message> {
+        "Hello, World !".into()
+    }
 }
