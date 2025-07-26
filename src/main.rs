c7b5dc49895125212bd2a4b6c98fe828d197a235
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-26 18:02:06 +0200

    use a scrollable with 2 chessboard
    so demonstrating that using several chessboards is possible

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 mod gui;
 use gui::widgets::chessboard::Chessboard;
-use iced::widget::column;
+use iced::widget::{Scrollable, column};
 
 fn main() -> iced::Result {
     iced::run("Chess against engine", App::update, App::view)
@@ -18,12 +18,12 @@ impl App {
     fn update(&mut self, _message: Message) {}
 
     fn view(&self) -> iced::Element<Message> {
-        column![
+        Scrollable::new(column![
             Chessboard::new_from_position(
                 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2".to_string(),
             ),
             Chessboard::new()
-        ]
+        ])
         .into()
     }
 }
