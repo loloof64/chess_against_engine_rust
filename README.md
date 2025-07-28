7d84bc0fda22914710321b774f99b74817753e05
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-28 14:55:18 +0200

    add toolbar and toggle orientation button

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Play chess against the device.
 ### Chess vectors
 
 Chess svg vectors have been downloaded from [Wikimedia Commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces).
+
+### SvgRepo
+
+Some vectors have been download from [SvgRepo](https://www.svgrepo.com)
+
+- [swap-vert.svg](https://www.svgrepo.com/svg/432595/swap-vert)
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,69 @@
 #![windows_subsystem = "windows"]
 
 mod gui;
-use gui::widgets::chessboard::Chessboard;
-use iced::{Length, widget::container};
+use std::sync::LazyLock;
 
-use crate::gui::widgets::chessboard::ChessboardOptions;
+use gui::widgets::chessboard::Chessboard;
+use iced::{
+    Background, Color, Length,
+    alignment::{Horizontal, Vertical},
+    widget::{Svg, button, column, container, row, svg::Handle},
+};
+
+use crate::gui::widgets::chessboard::ChessboardOptionsBuilder;
+
+static SWAP_VERT_BYTES: &[u8] = include_bytes!("swap-vert.svg");
+static SWAP_VERT_HANDLE: LazyLock<Handle> = LazyLock::new(|| Handle::from_memory(SWAP_VERT_BYTES));
 
 fn main() -> iced::Result {
     iced::run("Chess against engine", App::update, App::view)
 }
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    ToggleBoardOrientation,
+}
 
 #[derive(Default)]
-struct App {}
+struct App {
+    board_reversed: bool,
+}
 
 impl App {
-    fn update(&mut self, _message: Message) {}
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ToggleBoardOrientation => self.board_reversed = !self.board_reversed,
+        }
+    }
 
     fn view(&self) -> iced::Element<Message> {
-        container(Chessboard::new(ChessboardOptions::default()))
+        column![
+            container(
+                row![
+                    button(Svg::new(SWAP_VERT_HANDLE.clone()))
+                        .width(50)
+                        .height(50)
+                        .on_press(Message::ToggleBoardOrientation)
+                ]
+                .spacing(15.0)
+                .align_y(Vertical::Center),
+            )
+            .padding(5.0)
+            .center_x(Length::Fill)
+            .style(|_| container::Style {
+                background: Some(Background::Color(Color::from_rgb8(120, 120, 120))),
+                ..Default::default()
+            }),
+            container(Chessboard::new(
+                ChessboardOptionsBuilder::new()
+                    .set_reversed(self.board_reversed)
+                    .build(),
+            ))
             .center(Length::Fill)
-            .into()
+        ]
+        .align_x(Horizontal::Center)
+        .padding(10)
+        .spacing(10)
+        .into()
     }
 }
